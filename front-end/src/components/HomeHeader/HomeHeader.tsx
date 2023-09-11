cf813b445ebf703cea285f0c2b8dfac6250d3f3b
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-11 11:51:06 -0300

    Add a lot of changes to the front end

--- front-end/src/components/HomeHeader/HomeHeader.tsx
+++ front-end/src/components/HomeHeader/HomeHeader.tsx
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 
 export const HomeHeader = () => {
   return (
-    <header className="h-[50px] grid grid-rows-1 grid-cols-2 items-center px-16 bg-gradient-to-b from-neutral-100 to-neutral-200">
-      <Link to="/">
-        <h1 className="text-3xl opacity-90">Find the Pokemon</h1>
+    <header className="h-[50px] grid grid-rows-1 grid-cols-1 items-center px-16 bg-gradient-to-b from-neutral-100 to-neutral-200">
+      <Link
+        className="justify-self-center justify-center row-start-1 row-end-2 col-start-1 col-end-2"
+        to="/"
+      >
+        <h1 className="text-4xl opacity-90">Find the Pokemon</h1>
       </Link>
-      <div className="flex items-center justify-self-end gap-6">
+      <div className="flex items-center justify-self-end gap-6 row-start-1 row-end-2 col-start-1 col-end-2">
         <Link to="leaderboard">
           <h3 className="text-xl opacity-80">Leaderboard</h3>
         </Link>
--- front-end/src/components/HomeMainContent/HomeMainContent.tsx
+++ front-end/src/components/HomeMainContent/HomeMainContent.tsx
@@ -1,36 +1,41 @@
+import { Link } from "react-router-dom";
+
 export const HomeMainContent = () => {
   return (
-    <main className="h-[calc(100dvh-75px)] grid items-center bg-gradient-to-b from-neutral-300 to-neutral-400 px-16">
+    <main className="h-[calc(100dvh-75px)] grid items-center justify-items-center bg-gradient-to-r from-violet-500 to-fuchsia-500 text-violet-50 px-16">
       <div className="w-fit h-fit grid grid-rows-[auto_1fr] gap-6 auto-cols-fit">
-        <h3 className="text-3xl font-semibold opacity-[85%] col-start-1 col-end-3">
+        <h3 className="text-3xl font-semibold opacity-[75%] col-start-1 col-end-3 row-start-1 row-end-2 justify-self-center">
           Choose a map
         </h3>
 
-
         <div className="flex flex-col row-start-2 row-end-3 col-start-1 col-end-2 gap-2">
-          <h4 className="text-lg">Pallet Town</h4>
+          <h4 className="text-xl font-semibold opacity-70">Pallet Town</h4>
           <img
             src="/images/pokemon-bg.png"
             alt="an amalgamation ok pokemons"
             width={200}
             height={250}
           />
-          <button className="w-min h-min px-16 py-4 rounded bg-blue-500 text-blue-50 text-lg font-semibold">
-            Start
-          </button>
+          <Link to="/pallet-town">
+            <button className="w-min h-min px-16 py-4 rounded bg-blue-500 text-blue-50 text-lg font-semibold">
+              Start
+            </button>
+          </Link>
         </div>
+
         <div className="flex flex-col row-start-2 row-end-3 col-start-2 col-end-3 gap-2">
-          <h4 className="text-lg">fsdfsadsa</h4>
+          <h4 className="text-xl font-semibold opacity-70">fsdfsadsa</h4>
           <img
-            
             src="/images/pokemon-bg2.png"
             alt="an amalgamation ok pokemons"
             width={400}
             height={250}
           />
-          <button className="w-min h-min px-16 py-4 rounded bg-blue-500 text-blue-50 text-lg font-semibold">
-            Start
-          </button>
+          <Link to="/idk">
+            <button className="w-min h-min px-16 py-4 rounded bg-blue-500 text-blue-50 text-lg font-semibold">
+              Start
+            </button>
+          </Link>
         </div>
       </div>
     </main>
