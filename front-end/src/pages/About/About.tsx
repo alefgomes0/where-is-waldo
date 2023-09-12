392e7e7258c51f293a164141ea8a3c721aeed16e
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-12 12:35:19 -0300

    Put more content at About page

--- front-end/src/pages/About/About.tsx
+++ front-end/src/pages/About/About.tsx
@@ -5,7 +5,7 @@ export const About = () => {
     <>
       <Header />
       <main className="grid grid-cols-1 grid-rows-1 h-[calc(100dvh-75px)] bg-gradient-to-r from-sky-500 to-indigo-500 px-16">
-        <div className="text-2xl text-neutral-300  mt-16">
+        <div className="flex flex-col gap-6 text-2xl text-neutral-300  mt-16">
           <h3>
             This is a project, based on the{" "}
             <a
@@ -24,6 +24,37 @@ export const About = () => {
               The Odin Project
             </a>{" "}
             webstack curriculum.
+          </h3>
+          <h3>
+            The first map (Pallet Town) was made by{" "}
+            <a
+              href="https://www.reddit.com/user/LordLibidan/"
+              rel="noreferrer"
+              className="underline decoration-slate-700 underline-offset-4"
+            >
+              LordLibidan
+            </a>{" "}
+          </h3>
+          <h3>
+            The second map (Viridian City) was taken from{" "}
+            <a
+              href="https://www.reddit.com/user/LordLibidan/"
+              rel="noreferrer"
+              className="underline decoration-slate-700 underline-offset-4"
+            >
+              WallpaperDog
+            </a>{" "}
+          </h3>
+          <h3>Pokemon is a trademark of Nintendo</h3>
+          <h3>
+            Project made by{" "}
+            <a
+              href="http://alefg.com"
+              rel="noreferrer"
+              className="underline decoration-slate-700 underline-offset-4"
+            >
+              Alexandre Gomes
+            </a>
           </h3>
         </div>
       </main>
