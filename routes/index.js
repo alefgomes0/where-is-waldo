875ca54df52f50b7727566b9a1d2aa952e82f142
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-09-13 09:41:04 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const pokemonController = require("../controllers/pokemonController");
 const leaderboard = require("../controllers/leaderboard")
 
+router.get("/", res.send("hello"))
+
 router.get("/pokemon/:cityName", pokemonController.get);
 router.post("/pokemon", pokemonController.post);
 
