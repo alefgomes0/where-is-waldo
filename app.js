4bffbbfd7a0eea3290c5a484e9b58b1bf7feb10a
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-10-25 15:36:47 -0300

    dfopdsf

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use("/", indexRouter);
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/front-end/index.html"))
 })
 
 app.use((err, req, res, next) => {
