700782577188f33c176a4dfce03cb6615f79c088
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-10-25 14:57:15 -0300

    !

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const pokemonController = require("../controllers/pokemonController");
-const leaderboard = require("../controllers/leaderboard")
-const path = require("path")
+const leaderboard = require("../controllers/leaderboard");
+const path = require("path");
 
-router.get("/", (req,res, send) => res.send("hello"))
+router.get("/", (req, res, send) => res.send("hello"));
 
 router.get("/pokemon/:cityName", pokemonController.get);
 router.post("/pokemon", pokemonController.post);
 
-router.get("/leaderboard/:map", leaderboard.get)
-router.post("/leaderboard", leaderboard.post)
-
-router.get("/*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../front-end/index.html"))
-})
+router.get("/leaderboard/:map", leaderboard.get);
+router.post("/leaderboard", leaderboard.post);
 
 module.exports = router;
--- routes/teste.js
+++
@@ -1,4 +0,0 @@
-const path = require("path")
-
-const thisPath = path.join(__dirname, "../front-end/index.html")
-console.log(thisPath)
